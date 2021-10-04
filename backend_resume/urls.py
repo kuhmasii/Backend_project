b56361a889a9586557aaceb4f5ad3a504a3e9033
isaiah olaoye <olaisaiah54@gmail.com>
2021-10-04 01:00:38 +0100

    Added http404 template and updated the views file

--- backend_resume/urls.py
+++ backend_resume/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('resume.urls', namespace="resume"))
 ]
+handler404 = "resume.views.page_404"
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- resume/views.py
+++ resume/views.py
@@ -1,8 +1,8 @@
 import collections
 from django.contrib.auth import get_user, login, logout, authenticate
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import Http404
 from django.test import client
 from .models import *
 from . import forms
@@ -13,28 +13,28 @@ def index(request, *args, **kwargs):
     ins = None
     try:
         details = Detail.objects.get(pk=1)
-        project = Project.objects.all()
-        accom = Acomplishment.objects.get(pk=1)
+        project_ = Project.objects.all()
+        accomplish = Acomplishment.objects.get(pk=1)
         skills = [x.strip(",") for x in details.skills.split()]
         ins = Project.objects.get(pk=1)
-    except:
-        print("This request was invalid, will you want to try again later.")
+    except (Detail.DoesNotExist, Acomplishment.DoesNotExist):
+        raise Http404
     return render(request, "resume/index.html", dict(
-        details=details, project=project, accom=accom, skills=skills,
+        details=details, project=project_, accom=accomplish, skills=skills,
         ins=ins
     )
                   )
 
-
-
 def project(request, detail_pk, *args, **kwargs):
-    project = Project.objects.get(pk=detail_pk)
-
-    return render(request, 'resume/project.html', 
-                dict(
-                    project=project
+    try:
+        project_ = Project.objects.get(pk=detail_pk)
+    except Project.DoesNotExist:
+        raise Http404
+    return render(request, 'resume/project.html',
+                  dict(
+                      project=project_
+                  )
                 )
-            )
 
 def sign_account(request, *args, **kwargs):
     form = forms.SignInForm()
@@ -66,12 +66,11 @@ def sign_account(request, *args, **kwargs):
                   dict(
                       form=form,
                       login_error=login_error,
-                    )
-                )
+                  )
+                  )
 
 @login_required
 def dashboard(request, *args, **kwargs):
-    
     info = Detail.objects.get(personal_detail=get_user(request))
     info2 = Acomplishment.objects.get(pk=1)
 
@@ -84,27 +83,27 @@ def dashboard(request, *args, **kwargs):
         # Explicitly placing request.FILES helps to pick up the 
         # uploaded file in the form. without this, uploaded
         # files(image, file etc) won't be read
-        form = forms.ProjectForm(data=request.POST,files=request.FILES)
+        form = forms.ProjectForm(data=request.POST, files=request.FILES)
 
         if form.is_valid():
             name, topic = form.cleaned_data.get("name"), form.cleaned_data.get("topic")
-            desc,created= form.cleaned_data.get("description"),form.cleaned_data.get("created")
+            desc, created = form.cleaned_data.get("description"), form.cleaned_data.get("created")
             proj_url, client = form.cleaned_data.get("proj_url"), form.cleaned_data.get("client")
-            
+
             if 'proj_pic' in request.FILES:
                 proj_pic = request.FILES.get('proj_pic')
-            
-            ins = Project(name=name, 
-                    topic=topic,
-                    description=desc,
-                    proj_pic=proj_pic, 
-                    proj_url=proj_url,
-                    client=client, 
-                    project_created=created 
-                )
-            
+
+            ins = Project(name=name,
+                          topic=topic,
+                          description=desc,
+                          proj_pic=proj_pic,
+                          proj_url=proj_url,
+                          client=client,
+                          project_created=created
+                          )
+
             ins.save()
-            
+
             return redirect("resume:index")
 
     if request.method == "POST":
@@ -112,27 +111,30 @@ def dashboard(request, *args, **kwargs):
             instance=info,
             data=request.POST,
             files=request.FILES
-            )
+        )
         form_upd_accom = forms.AccomUpdateForm(
             instance=info2,
             data=request.POST
         )
         if form_update.is_valid() and form_upd_accom.is_valid():
-                form_update.save()
-                form_upd_accom.save()
-                return redirect("resume:index")
+            form_update.save()
+            form_upd_accom.save()
+            return redirect("resume:index")
 
     return render(
-        request, 
-        "resume/dashboard.html", 
+        request,
+        "resume/dashboard.html",
         dict(
-            form=form, 
+            form=form,
             form_update=form_update,
             form_upd_accom=form_upd_accom
-            )
         )
+    )
 
 @login_required
 def sign_out_account(request, *args, **kwargs):
     logout(request)
     return redirect("resume:index")
+
+def page_404(request, exception):
+    return render(request, "404.svg")
