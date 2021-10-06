bd842689b657b90fe041b7f6b4d7cc156fac2df6
isaiah olaoye <olaisaiah54@gmail.com>
2021-10-06 01:58:42 +0100

    db

--- backend_resume/settings.py
+++ backend_resume/settings.py
@@ -85,14 +85,14 @@ WSGI_APPLICATION = 'backend_resume.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
