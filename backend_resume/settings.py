9be91ecac22770c02223103c6472d0745c333c32
isaiah olaoye <olaisaiah54@gmail.com>
2021-10-06 09:47:44 +0100

    still updating settings file

--- backend_resume/settings.py
+++ backend_resume/settings.py
@@ -168,3 +168,9 @@ if ENV == 'production':
     # import dj_database_url
     
     # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
