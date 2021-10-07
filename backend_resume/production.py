7724c84b94c1fc5ce26ea3e0d021302465f82f24
isaiah olaoye <olaisaiah54@gmail.com>
2021-10-07 14:00:55 +0100

    modified allowhost

--- backend_resume/production.py
+++ backend_resume/production.py
@@ -1,7 +1,7 @@
 
 import os
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.heroku.com', 'kuhmasii.heroku.com']
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = int(os.environ.get('DEBUG'))
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
