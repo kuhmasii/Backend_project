4619681594a67e4781f87ac208b2a814832dca06
kuhmasii <olaisaiah54@gmail.com>
2022-05-08 14:36:58 +0100

    change the fields attribute namea and the values

--- resume/models.py
+++ resume/models.py
@@ -3,71 +3,91 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 # Create your models here.
+
+
 class Detail(models.Model):
-    personal_detail = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
-    phone_number = models.CharField(max_length=16, blank=True)
-    address = models.CharField(max_length=50, blank=True)
-    profile_pic = models.ImageField(upload_to='my_pics/', blank=False)
-    backgroud_pic = models.ImageField(upload_to='my_pics/', blank=False)
-    about_me = models.TextField(blank=True)
-    skills = models.CharField(max_length=150, blank=False)
+    personal_detail = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=False)
+    phone_number = models.CharField(max_length=16, blank=True, null=True)
+    address = models.CharField(max_length=50, blank=True, null=True)
+    _profile_pic = models.ImageField(
+        upload_to='my_pics/', default="my_pics/avatar.png", null=True, blank=True)
+    _backgroud_pic = models.ImageField(
+        upload_to='my_pics/', default="my_pics/avatar.png", null=True, blank=True)
+    about_me = models.TextField(blank=True, null=True)
+    skills = models.CharField(max_length=150, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.personal_detail.username} is Your Username, {self.personal_detail.first_name} {self.personal_detail.last_name}"
+        return self.personal_detail.username
 
     @property
-    def profileURL(self):
+    def profile_pic(self):
         try:
-            url = self.profile_pic.url
+            url = self._profile_pic.url
         except ValueError:
             url = ''
         return url
 
     @property
-    def backgroudURL(self):
+    def backgroud_pic(self):
         try:
-            url = self.backgroud_pic.url
+            url = self._backgroud_pic.url
         except ValueError:
             url = ''
         return url
+
+    def get_projects(self):
+        projects = self.project_owner.all()
+        return projects
+
 
 class Acomplishment(models.Model):
-    personal_detail = models.ForeignKey(Detail, on_delete=models.CASCADE, related_name="acomplishment")
-    work_completed = models.IntegerField(default=0, help_text='Total Work Completed')
-    years_of_exper = models.IntegerField(default=0, help_text='Years of Experience.')
-    total_client = models.IntegerField(default=0, help_text='Total Client Worked For.')
-    award_won = models.IntegerField(default=0, help_text="Award Won.")
+    work_completed = models.PositiveIntegerField(
+        default=0, help_text='Total Work Completed')
+    years_of_exper = models.PositiveIntegerField(
+        default=0, help_text='Years of Experience.')
+    total_client = models.PositiveIntegerField(
+        default=0, help_text='Total Client Worked For.')
+    award_won = models.PositiveIntegerField(default=0, help_text="Award Won.")
 
     def __str__(self):
-        return  str(self.pk)
+        return str(self.pk)
+
+    @property
+    def accomplishment_update(self):
+        projects = Project.objects.all()
+
+        project_count = projects.count()
+        total_client = projects.exclude(
+            client__iexact="Personal Project").count()
+
+        self.work_completed = project_count
+        self.total_client = total_client
+        self.years_of_exper = 2
+
+        self.save()
+
 
 class Project(models.Model):
+    owner = models.ForeignKey(
+        Detail, on_delete=models.SET_NULL, null=True, related_name="project_owner")
     name = models.CharField(max_length=150, blank=False)
     topic = models.CharField(max_length=50, blank=False)
-    description = models.TextField(blank=True)
-    proj_pic = models.ImageField(upload_to='my_project_pics/', blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    _proj_pic = models.ImageField(
+        upload_to='my_project_pics/', default="my_project_pics/cover.jpg", null=True, blank=True)
     proj_url = models.URLField()
-    client = models.CharField(max_length=100, blank=True)
+    client = models.CharField(max_length=100, blank=True, null=True)
     project_created = models.DateTimeField()
 
     def __str__(self):
         return self.name
 
-    def get_total_accom(self):
-        return len(
-            Project.objects.all()
-        )
-    
-    def get_total_client(self):
-        ins = Project.objects.all()
-        total_client = [x for x in ins if x.client != "Personal Project"]
-        return len(total_client)
-
     @property
-    def ProjectURL(self):
+    def proj_pic(self):
         try:
-            url = self.proj_pic.url
+            url = self._proj_pic.url
         except ValueError:
             url = ''
         return url
